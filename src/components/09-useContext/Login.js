91e0f6bbfccbb381e16ad90054dbf872f13f243d
Adrián Montero <hernandezmonteroadrian14@gmail.com>
2022-02-05 02:51:24 -0600

    Login button valdiated

--- src/components/09-useContext/Login.js
+++ src/components/09-useContext/Login.js
@@ -2,19 +2,26 @@ import React, { useContext } from "react";
 import { UserContext } from "./UserContext";
 
 const Login = () => {
-  const { setUser } = useContext(UserContext);
+  const {
+    user: { id, name },
+    setUser
+  } = useContext(UserContext);
   const newUser = { id: 7, name: "Adrián Montero", email: "adrian@gmail.com" };
 
   return (
     <div>
       <h1>Login</h1>
       <hr />
-      <button
-        className="btn btn-outline-primary"
-        onClick={() => setUser(newUser)}
-      >
-        Get user
-      </button>
+      {!id ? (
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => setUser(newUser)}
+        >
+          Get user
+        </button>
+      ) : (
+        <p>Hi, {name}!</p>
+      )}
     </div>
   );
 };
